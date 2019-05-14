3d686232ab14c1c9ed7f397701d5e0389daa7a45
Jeremy Ng <11280413+KingJeremyNg@users.noreply.github.com>
2019-05-13 21:27:04 -0400

    Add files via upload

---
+++ Commands.js
@@ -0,0 +1,116 @@
+const { createCanvas, loadImage } = require("canvas");
+var fs = require("fs");
+
+function log(msg) {
+    return (msg.author.tag + " | " + msg.content);
+};
+
+function separate(string) {
+    return string.match(/\S+/g) || []
+};
+
+function randomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+module.exports =  {
+
+    cookie : function(msg) {
+        console.log(log(msg));
+        return ":cookie:";
+    },
+
+    yar : function(msg) {
+        console.log(log(msg));
+        return "YARFILE";
+    },
+
+    ping : function(msg) {
+        console.log(log(msg));
+        return "Pong!"
+    },
+
+    say : function(msg) {
+        console.log(log(msg));
+        var array = separate(msg.content);
+        array.splice(0,1);
+        return array.join(" ");
+    },
+
+    shutdown : function(msg) {
+        console.log(log(msg));
+        process.exit();
+    },
+
+    mock : function(msg) {
+        console.log(log(msg));
+        var array = separate(msg.content);
+        array.splice(0,1);
+        var string = "";
+        var temp = array.join(" ");
+        temp = temp.split("");
+        for (var char in temp) {
+            random = randomInt(0,1);
+            random ? string += temp[char].toUpperCase() : string += temp[char].toLowerCase();
+        }
+        return string;
+    },
+
+    roll : function(msg) {
+        console.log(log(msg));
+        var array = separate(msg.content);
+        array.splice(0,1);
+        switch(array.length) {
+            case 1:     if (!isNaN(array[0])) {
+                            return `You rolled ${ array[0] } dice for ${ randomInt(1 * array[0], 6 * array[0]) }`;
+                        }
+            case 2:     if (!isNaN(array[0]) && !isNaN(array[1])) {
+                            return `You rolled a d${ array[1] } * ${ array[0] } for ${ randomInt(1 * array[0], array[0] * array[1]) }`;
+                        }
+            default:    return `You rolled ${ randomInt(1, 6) }`;
+        }
+    },
+
+    boi : function(msg) {
+        console.log(log(msg));
+        return ('./boi.png');
+    },
+
+    ahshit : async function(msg) {
+        console.log(log(msg));
+        var array = separate(msg.content);
+        array.splice(0,1);
+
+        await loadImage(msg.author.avatarURL).then(async (image) => {
+            const canvas = createCanvas(image.width, image.height);
+            var ctx = canvas.getContext('2d');
+            ctx.drawImage(image, 0, 0, image.width, image.height);
+
+            await loadImage('./ahshit.png').then(async (meme) => {
+                var scale = Math.min(image.height / meme.height, image.width / meme.width);
+
+                ctx.drawImage(meme, image.width - (meme.width * scale), image.height - (meme.height * scale), meme.width * scale, meme.height * scale);
+
+                await canvas.toBuffer((err, Buf) => {
+                    if (err) {
+                        return err;
+                    }
+                    else {
+                        return Buf;
+                    }
+                });
+
+                //var buffer = canvas.toBuffer();
+                //await fs.writeFileSync('ahshitmeme.png', buffer);
+            })
+        })
+        //return './ahshitmeme.png';
+    },
+
+    protect : function(msg) {
+
+    },
+
+}
---
+++ JeremyBot.js
@@ -0,0 +1,56 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const Kitsu = require("kitsu");
+const kitsuAPI = new Kitsu();
+
+const Commands = require("./Commands");
+
+const Settings = require('./Settings');
+
+var fs = require("fs");
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("message", msg => {
+    var line = msg.content.match(/\S+/g) || []
+    command = String(line[0]).toUpperCase();
+    switch(command) {
+        case "COOKIE":  msg.channel.send(Commands.cookie(msg));
+                        break;
+
+        case "YAR":     msg.channel.send(Commands.yar(msg));
+                        break;
+
+        case "!PING":   msg.reply(Commands.ping(msg));
+                        break;
+
+        case "!SAY":    msg.channel.send(Commands.say(msg));
+                        break;
+
+        case "!MOCK":   msg.channel.send(Commands.mock(msg));
+                        break;
+
+        case "!ROLL":   msg.channel.send(Commands.roll(msg));
+                        break;
+
+        case "!PROTECT":msg.channel.sendFile(Commands.protect(msg));
+                        break;
+
+        case "BOI":     msg.channel.sendFile(Commands.boi(msg));
+                        break;
+        
+        case "!AHSHIT": //let attachment = new Discord.Attachment(Commands.ahshit(msg));
+                        msg.channel.sendFile(Commands.ahshit(msg));
+                        //fs.unlink('./ahshitmeme.png');
+                        break;
+
+        case "!HELP":   break;
+
+        case "!QUIT":   Commands.shutdown(msg);
+    }
+});
+
+client.login(Settings.token);
