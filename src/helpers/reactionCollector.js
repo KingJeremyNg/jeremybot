94a9f94107cb3d84c35e26d6fd7e3431f1ceae1c
KingJeremyNg <jeremy97ng@gmail.com>
2020-01-22 21:21:13 -0500

    reactionCollector improvements

--- src/helpers/reactionCollector.js
+++ src/helpers/reactionCollector.js
@@ -3,26 +3,27 @@ async function reactionCollector(msg, client, num, emoji, time) {
         return reaction.emoji.name === emoji;
     }
 
-    msg.react(emoji);
-    msg.channel.send(`> <@!${msg.author.id}> has started a headcount!\n> Collection will time out in ${time / 60000} minutes.`);
+    return await msg.channel.send(`> <@!${msg.author.id}> has started a headcount here!\n> Collection will time out in ${time / 60000} minutes.\n> React here with ${emoji}.`)
+        .then(sentMessage => {
+            sentMessage.react(emoji)
 
-    
-    function removeReaction(msg, client, emoji) {
-        msg.reactions.get(emoji).remove(client.user.id);
-    }
-    setTimeout(removeReaction, 60000, msg, client, emoji);
+            function removeReaction(sentMessage, client, emoji) {
+                sentMessage.reactions.get(emoji).remove(client.user.id);
+            }
+            setTimeout(removeReaction, 60000, sentMessage, client, emoji);
 
-    return await msg.awaitReactions(filter, { max: num + 1, time: time, errors: ['time'] })
-        .then(collected => {
-            let map = collected.get(emoji).users.keys();
-            let idList = Array.from(map);
-            idList.splice(idList.indexOf(client.user.id), 1);
-            msg.channel.send("@here headcount completed!");
-            return idList;
-        })
-        .catch(collected => {
-            msg.channel.send(`After 1 hour, only ${collected.size} out of ${num} reacted.`);
-        })
+            return sentMessage.awaitReactions(filter, { max: num + 1, time: time, errors: ['time'] })
+                .then(collected => {
+                    let map = collected.get(emoji).users.keys();
+                    let idList = Array.from(map);
+                    idList.splice(idList.indexOf(client.user.id), 1);
+                    msg.channel.send("Headcount completed!");
+                    return idList;
+                })
+                .catch(collected => {
+                    msg.channel.send(`After 1 hour, only ${collected.size} out of ${num} reacted.`);
+                })
+        });
 }
 
 //TODO: UNIQUE REACTIONS COUNT ONLY
